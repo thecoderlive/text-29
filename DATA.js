ba9f6d0b2c7e7228a31da13e509ae09d069b1500
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 19:39:19 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "a7822d52-fea7-4487-a395-2a9a38b76d8a",
+    "home_feed": [
+        {
+            "id": "3956476d-61cb-462e-bf2b-8bcd1bd8bfbd",
+            "artist_name": "agency",
+            "home_carousel": [
+                {
+                    "id": "64953e31-dd33-499f-9265-db603e5d4735",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                },
+                {
+                    "id": "a72cb69c-1e5a-4d21-b0ba-e74d86a5753a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "a2c40083-bd4d-499a-a7b0-3ee31080a7a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png"
+                },
+                {
+                    "id": "b509faa4-8da0-4040-9513-a61ba99e88d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "2af5ef39-7649-4519-abe7-1710d3e84a51",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                }
+            ],
+            "art_caption": "will",
+            "art_description": "Top year company collection matter. Eye company audience card talk. Represent ten detail tend executive improve board."
+        },
+        {
+            "id": "497cba8e-903a-491d-9042-896223fd27dd",
+            "artist_name": "yes",
+            "home_carousel": [
+                {
+                    "id": "8beb7ec7-63fa-46c1-a44d-a2b6db8431a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png"
+                },
+                {
+                    "id": "68e39c07-ceac-4e5e-8824-f9345067a102",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "0b0342ab-1361-49cb-819b-1aaa37ca6a77",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                },
+                {
+                    "id": "d11ccb5f-28a9-41a7-824b-36a5fb3dbe4f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                },
+                {
+                    "id": "18bc145f-3d3b-42dd-834b-471789ab6f5d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png"
+                }
+            ],
+            "art_caption": "about",
+            "art_description": "Worry sort pattern better economy coach. Realize show exist whose."
+        },
+        {
+            "id": "28306062-f480-464b-a55f-41f3587f3a3a",
+            "artist_name": "total",
+            "home_carousel": [
+                {
+                    "id": "a9f30665-67de-45f8-9147-c46d80634995",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "8a36d6a5-1c6d-4f32-a756-4dae67210516",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "fadfe1a7-f0e4-4e34-9596-30dbe36d2f74",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                },
+                {
+                    "id": "567a2590-7c77-4ace-beb4-4859c2347487",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "2c9aab8b-7ab7-42f7-ac99-5318bc5c2f28",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                }
+            ],
+            "art_caption": "floor",
+            "art_description": "Value wide much conference late follow. Rest throw push. Black deep all child hear effort."
+        },
+        {
+            "id": "f6b38e23-cacb-4450-9d9d-d3571bcb0dfc",
+            "artist_name": "recently",
+            "home_carousel": [
+                {
+                    "id": "92bdf3d5-010c-41f7-944e-7efbfc32224b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "61f22a54-27b6-4a07-9246-5964ab96aef6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/250.png"
+                },
+                {
+                    "id": "fdcdcdaf-277e-469f-9a2d-a85bfed99fbe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "ee2098ed-d99a-4072-bbde-1be8e6f19e34",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png"
+                },
+                {
+                    "id": "be8344aa-3cb4-418d-836e-704e8750837d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                }
+            ],
+            "art_caption": "mission",
+            "art_description": "Notice decade TV agent late standard table. Away statement toward model good."
+        },
+        {
+            "id": "ab7adb4e-57a1-4153-bdb1-bff1a7963ce6",
+            "artist_name": "language",
+            "home_carousel": [
+                {
+                    "id": "9f79b1f4-8809-4243-a411-e486d88693b9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                },
+                {
+                    "id": "6191236a-4fb5-440f-bc83-8fdca5688e05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/207.png"
+                },
+                {
+                    "id": "de576393-de55-4b09-b201-3ff4e08ab8ee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                },
+                {
+                    "id": "2faf289e-699a-4b78-b878-c1e84919fc1d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "5a3b4ebf-6be8-4917-b83a-1ec4b5161b38",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                }
+            ],
+            "art_caption": "nation",
+            "art_description": "By onto food feeling Mrs marriage both. Leave social play study."
+        }
+    ]
+}
